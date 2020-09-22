e340141cc4701d192e10112db1aff893456b2e2e
syedkhamer <42810493+syedkhamer@users.noreply.github.com>
2020-09-23 01:51:33 +0530

    Update header.component.ts
    Adding changes to make NavBar Dynamic.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs/internal/observable/fromEvent';
+import { throttleTime,map,pairwise,distinctUntilChanged, share ,filter } from 'rxjs/operators';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('toggle', [
+      state(
+        VisibilityState.Hidden,
+        style({ opacity: 0, transform: 'translateY(-100%)' })
+      ),
+      state(
+        VisibilityState.Visible,
+        style({ opacity: 1, transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('300ms ease-in'))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
+  
+  private isVisible = true;
+
+  @HostBinding('@toggle')
+  get toggle(): VisibilityState {
+    return this.isVisible ? VisibilityState.Visible : VisibilityState.Hidden;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+
+    const scrollUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const scrollDown = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    scrollUp$.subscribe(() => (this.isVisible = true));
+    scrollDown.subscribe(() => (this.isVisible = false));
   }
 
 }
